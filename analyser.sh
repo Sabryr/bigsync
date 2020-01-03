9c9c1c14f1e97b49ea5efa206bdd1a91cc0b3f14
Sabry Razick <sabry.razick@usit.uio.no>
2020-01-03 09:19:28 +0100

    imitial commit

---
+++ analyser.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+location="$1"
+results="results.log"
+progress="$2"
+for dir in $(ls $location)
+do 
+	if grep -q "$location$dir" "$progress"
+	then 
+		echo "found" 
+	else 
+		echo "$location$dir" >> "$progress"
+		du -hs "$location$dir"  >> "results"
+		sleep 2
+	fi	
+ 	#tar cf "$dir.tar" "/mnt/cees/in_progress/$dir"
+done
+
+#if [[ -f my.tr ]]
+#then 
+#	echo "found" 
+#else 
+#	echo "not found"
+#fi

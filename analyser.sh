bd60ef49fd353d5e5cd014308e32ca05abbab1ad
Sabry Razick <sabryr@backup01.abel.uio.no>
2020-01-06 08:42:01 +0100

    Folder level analyser

--- analyser.sh
+++ analyser.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+#sabryr 2020-01-03
+#To calculate the space usage of
+#very large folders 
 
 location="$1"
 results="results.log"
 progress="$2"
 for dir in $(ls $location)
 do 
-	if grep -q "$location$dir" "$progress"
+	if grep -q $location"/"$dir "$progress"
 	then 
 		echo "found" 
 	else 
-		echo "$location$dir" >> "$progress"
-		du -hs "$location$dir"  >> "results"
+		echo $location"/"$dir >> "$progress"
+		du -hs $location"/"$dir  >> "$results"
 		sleep 2
 	fi	
  	#tar cf "$dir.tar" "/mnt/cees/in_progress/$dir"

64b5cc5c22e6d8a62d30bd39c2a179ab22e9d91d
Sabry Razick <sabryr@gmail.com>
2020-01-09 15:14:43 +0100

    with scp as argument

--- tarsync.sh
+++ tarsync.sh
@@ -2,17 +2,15 @@
 #sabryr 2020-01-08
 #ToDO try backup01 as proxy
 #export PATH=/mnt/staff/sabry/scripts:$PATH
-LOCATION=$1 #"/mnt/cees/sabry/size/"
-#results="results.log"
-TARTARGET=$2  #"/mnt/cees/sabry/size/tmp/"
+MYNAME=$1
+LOCATION=$2 #"/mnt/cees/sabry/size/"
+TARTARGET=$3  #"/mnt/cees/sabry/size/tmp/"
 PROGRESS=$TARTARGET"progress.txt"
 touch $PROGRESS
-SCP_TARGET=""
+SCP_TARGET="$4"
+echo $4
+exit 1
 let DIR_NUM=$(ls -l $LOCATION | wc -l)-1
-
-location="$1"
-results="results.log"
-progress="$2"
 
 sync (){
 	Cdir=$1
@@ -20,8 +18,8 @@ sync (){
 	tar cf $Tdir $Cdir --hard-dereference &> /dev/null
 	checksum=$(sha1sum $Cdir | awk '{print substr($0,0,6)}')
  	mv $Tdir $Tdir-$checksum
- 	echo "$i of $DIR_NUM started " $(du -hs $Tdir-$checksum)
- 	echo scp $Tdir-$checksum $SCP_TARGET
+ 	echo "$i of $DIR_NUM started " $(du -hs $Tdir-$checksum) " by " $MYNAME
+ 	scp $Tdir-$checksum $SCP_TARGET
  	if [ $? -eq "0" ] ;
  	then
  		echo rm $Tdir-$checksum
@@ -33,20 +31,20 @@ PROGRESS=$TARTARGET"/progress"
 touch $PROGRESS
 i=0
 skipped=0
-for dir in $(ls $location)
+for dir in $(ls $LOCATION)
 do 
-	if grep -q $location"/"$dir $PROGRESS
+	if grep -q $LOCATION"/"$dir $PROGRESS
 	then 
 		echo "found" &> /dev/null
 		let skipped=$skipped+1
  	else 
+		echo $LOCATION"/"$dir >> $PROGRESS 
+		sync $LOCATION"/"$dir $TARTARGET"/"$dir".tar"
 		let i=$i+1
-		echo $location"/"$dir >> $PROGRESS 
-		sync $location"/"$dir $TARTARGET"/"$dir".tar"
-	 	echo "$i of $DIR_NUM done"
+		echo "$i of $dir done by "$MYNAME
 		sleep 2
 	fi	
  	#tar cf "$dir.tar" "/mnt/cees/in_progress/$dir"
 done
 
-echo $skipped " of "$DIR_NUM" skipped"
+echo $skipped " of "$DIR_NUM" skipped " 

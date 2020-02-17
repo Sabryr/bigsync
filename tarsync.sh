60140e5679280055da5cead6d93181ff6c4933d3
Sabry Razick <sabryr@gmail.com>
2020-02-17 09:52:43 +0100

    clean up scp part, the script will not do the scp part and stop after renaming file wil sha1sum

--- tarsync.sh
+++ tarsync.sh
@@ -2,26 +2,41 @@
 #sabryr 2020-01-08
 #ToDO try backup01 as proxy
 #export PATH=/mnt/staff/sabry/scripts:$PATH
-MYNAME=$1
-SRC_DIR=$2
-TAR_DIR=$3
-DIR=$4
-SCP_TARGET=$5
-sync (){
-	Cdir=$SRC_DIR"/"$DIR
-	Tdir=$TAR_DIR"/"$DIR".tar"
- 	echo  "$Cdir started by " $MYNAME
-	tar cf $Tdir $Cdir --hard-dereference &> /dev/null
-	checksum=$(sha1sum $Tdir | awk '{print substr($0,0,6)}')
-	mv $Tdir $Tdir-$checksum
- 	#scp $Tdir-$checksum $SCP_TARGET
- 	#if [ $? -eq "0" ] ;
- 	#then
- 	#	rm $Tdir-$checksum
- 	#fi
-	sleep 10
-	echo "Done " $Cdir
-	echo $Cdir >> $TAR_DIR"/done"
-}
+#tarsync.sh 1 /mnt/cees/x /mnt/staff/sabry/cees cdir null out.log
 
-sync $SRC_DIR $TAR_DIR
+if [ "$#" -ge 6 ]
+then
+	MYNAME=$1
+	SRC_DIR=$2
+	TAR_DIR=$3
+	DIR=$4
+	SCP_TARGET=$5
+	DONE=$6
+	touch $DONE
+	sync (){
+		Cdir=$SRC_DIR"/"$DIR
+		Tdir=$TAR_DIR"/"$DIR".tar"
+ 		echo  "$Cdir started by " $MYNAME
+		tar cf $Tdir $Cdir --hard-dereference &> /dev/null
+		if [ $? -eq "0" ] ;
+			checksum=$(sha1sum $Tdir | awk '{print substr($0,0,6)}')
+			mv $Tdir $Tdir-$checksum
+			echo "$Cdir -- OK ">> $DONE 
+		else
+			echo "--ERROR-- $Cdir -- Failed ">> $DONE 		
+		fi
+	}
+	sync $SRC_DIR $TAR_DIR
+else
+	echo "Need 6 arguments"
+fi
+
+  		#scp $Tdir-$checksum $SCP_TARGET
+	 	#if [ $? -eq "0" ] ;
+ 		#then
+	 	#	rm $Tdir-$checksum
+ 		#fi
+		sleep 10
+		echo "Done " $Cdir
+		echo $Cdir >> $DONE
+
